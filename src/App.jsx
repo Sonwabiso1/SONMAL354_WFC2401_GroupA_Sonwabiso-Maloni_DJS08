624f0d2ff63aa3f3d08a015a15869ef73a44982c
Sonwabiso <sonwabisolebokgang@gmail.com>
2024-06-09 22:54:16 +0200

    imported  vandetails and installed mirage js

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "../pages/Home"
 import About from "../pages/About"
 import Vans from "../pages/Vans"
+import VanDetail from "../pages/VanDetail"
+
+import "../server"
 
 function App() {
 
